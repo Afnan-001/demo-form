321fc02974bd027d63a3dd4c8054d1968ec48f65
Afnan-001 <afnansayyad413@gmail.com>
2025-11-15 10:39:28 +0530

    Title and description change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Demo Form',
-  description: 'A multi step form for user information',
+  title: 'Income Growth Application',
+  description: 'Explore financial opportunity options. Apply now to get started!',
 };
 
 export default function RootLayout({
