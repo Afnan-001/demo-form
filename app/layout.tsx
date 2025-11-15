e4dfe67bd0cf4f7f772937bac3ec270ba059c91e
Afnan-001 <afnansayyad413@gmail.com>
2025-11-15 10:42:08 +0530

    Title update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Income Growth Application',
+  title: 'Income Boost Form',
   description: 'Explore financial opportunity options. Apply now to get started!',
 };
 
